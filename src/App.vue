ebcaa5269428cf94de348db58d52b36a3bafd270
Jonas Anetzberger <jonas.anetzberger@smail.th-koeln.de>
2022-12-05 17:10:03 +0100

    added Header and footer to /App

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,12 @@
 <template>
   <div>
+    <Header />
     <router-view />
+    <Footer />
   </div>
 </template>
 
 <script setup lang="ts">
+import Header from "./components/Header.vue";
+import Footer from "./components/Footer.vue";
 </script>
