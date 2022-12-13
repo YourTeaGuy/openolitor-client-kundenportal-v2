47d2d2be0b5577680362927ceeca0735fcb86ead
Martin Ebendorff <m.ebendorff@googlemail.com>
2022-12-13 10:14:27 +0100

    files added

---
+++ src/components/composables/Label.vue
@@ -0,0 +1,16 @@
+<!-- reihenfolge ist mit absicht geändert, sonst gibts fehler! -->
+
+<script lang="ts">
+  import { defineComponent } from "vue";
+  export default defineComponent({
+    props: [
+      'text'
+    ]
+  })
+</script>
+
+<template>
+  <div>
+    {{ text }}
+  </div>
+<template/>
